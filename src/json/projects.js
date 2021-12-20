3a818c20bc6e3e94692aa609e46b080088ce76a2
Patel Rohan <60181322+patelrohan224@users.noreply.github.com>
2021-12-20 20:27:13 +0530

    Update projects.js

--- src/json/projects.js
+++ src/json/projects.js
@@ -10,15 +10,15 @@ export const projects = [
     disc:"Zoomcar is an Indian self-drive car rental company, headquartered in Bangalore, India. The company was founded in 2013 by David Back and Greg Moran, The company operates in 45 cities across the country. In this project, we have cloned the Zoomcar website and given some of the functionality as it is present in the original website"
     ,skill:"HTML | CSS | Javascript | ExpressJs | Node | MongoDB"
   },
-  {
-    img: "https://patelrohan224.github.io/rohanpatel/proj_zoomcar.PNG",
-    name: "ZoomCar clone",
+ {
+    img: "https://patelrohan224.github.io/rohanpatel/policybazaar.PNG",
+    name: "PolicyBazzar clone",
     ext: "jsx",
     lang: "js",
-    git: "https://github.com/patelrohan224/Zoomcar_EJS",
-    live: "https://zoomcarcom.herokuapp.com/home",
-    disc:"Zoomcar is an Indian self-drive car rental company, headquartered in Bangalore, India. The company was founded in 2013 by David Back and Greg Moran, The company operates in 45 cities across the country. In this project, we have cloned the Zoomcar website and given some of the functionality as it is present in the original website"
-    ,skill:"HTML | CSS | Javascript | ExpressJs | Node | MongoDB"
+    git: "https://github.com/patelrohan224/Policybazaar_clone",
+    live: "https://policybazaar-clone.netlify.app/",
+    disc:"Policybazaar is an Indian insurance aggregator and multinational financial technology company based in Gurgaon"
+    ,skill:"HTML | CSS | ReactJs | ExpressJs | NodeJs | MongoDB | Redux | JSON server | Material UI"
   },
     {
     img: "https://user-images.githubusercontent.com/60181322/133924928-c6e24b8d-2c17-46bd-8147-b897e3e6810b.png",
